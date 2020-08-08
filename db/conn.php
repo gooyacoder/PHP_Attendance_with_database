1ea637fef1a4a99dbb59d6567d16d1db470d14b0
Amirhosein Mahdiyar <amirhosein@localhost.localdomain>
2020-08-08 15:22:29 +0430

    Production database configured

--- db/conn.php
+++ db/conn.php
@@ -1,10 +1,19 @@
 <?php
 
-
+	//Development Database
 	$host = 'localhost';
 	$db = 'attendance_db';
 	$user = 'root';
 	$password = 'ahm';
+	$charset = 'utf8mb4';
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+	//Production Database
+	$host = 'sql308.epizy.com';
+	$db = 'epiz_26456153_a01';
+	$user = 'epiz_26456153';
+	$password = 'VFMblmGA45Boy';
 	$charset = 'utf8mb4';
 	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
